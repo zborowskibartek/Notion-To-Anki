1b4e79fabf923efd4dd6fca8dd8541050987f5ed
Bartosz Zborowski <zborowski.bartek@gmail.com>
2020-12-15 21:36:53 +0100

    Add FlashCard model to store question and answer

---
+++ src/main/java/FlashCard.java
@@ -0,0 +1,18 @@
+public class FlashCard {
+
+    private final String question;
+    private final String answer;
+
+    public FlashCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
